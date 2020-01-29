63f534d73b20390b55c4c1cd6cae3c58ab87c3d2
John <Thebohemiangeek1@gmail.com>
2020-01-29 17:51:34 +0100

    navbar completed

--- src/components/Globals/Navbar.js
+++ src/components/Globals/Navbar.js
@@ -12,8 +12,30 @@ const Navbar = () => {
 
   return (
     <nav className="navbar navbar-expand-sm bg-light navbar-light">
-<Link to="/" className="navbar-brand">
-    <img src={logo} alt="logo"/></Link>    </nav>
+      <Link to="/" className="navbar-brand">
+        <img src={logo} alt="logo" />
+      </Link>
+      <button className="navbar-toggler" type="button" onClick={toggleNav}>
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className={isOpen? "collapse navbar-collapse show" 
+        : "collapse navbar-collapse"}>
+        <ul className="navbar-nav mx-auto">
+            {
+                Links.map(Links =>{
+                    return(
+                    <li key={Links.id } className="nav-item" >
+                        <Link to={Links.path} className="nav-link text-capitalize">{Links.text}</Link>
+                    </li>
+                )})}
+                <li className="nav-item ml-sm-5">
+                    <FaCartArrowDown className="cart-icon"/>
+                   
+                </li>
+        </ul>
+
+      </div>
+    </nav>
   )
 }
 
