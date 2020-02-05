f3272b7de78fc5875632110ef9147a94881f91cd
John <Thebohemiangeek1@gmail.com>
2020-02-05 12:56:54 +0100

    contact form added

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import BackgroundHero from "../components/Globals/BackgroundHero"
 import Info from "../components/Home/info"
 import Menu from "../components/Home/Menu"
 import Products from "../components/Home/Products"
+import Contact from "../components/Home/Contact"
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
@@ -21,6 +22,7 @@ const IndexPage = ({ data }) => (
     <Info />
     <Menu items={data.menu} />
     <Products />
+    <Contact />
   </Layout>
 )
 export const query = graphql`
